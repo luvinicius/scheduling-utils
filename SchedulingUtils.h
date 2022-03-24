f002cff78e874a69397cd7750bf94e5644b51850
luvinicius <lucas.v.pimenta@gmail.com>
2022-03-24 09:05:19 -0300

    getDigitsOfYear method and tests

--- SchedulingUtils.h
+++ SchedulingUtils.h
@@ -83,22 +83,7 @@ extern "C++"
 
   int * getDigitsOfYear(int year);
 
-  bool isLeapYear(int year);
-
-  int numberOfDays(int year, int month);
-
-  typedef enum
-  {
-    NONE = 0,
-    SECONDS = 1,
-    MINUTES = 2,
-    HOURS = 3,
-    DAYS = 4,
-    MONTHS = 5,
-    YEARS = 6
-  } Duration;
-
-  tmElements_t sumToDate(tmElements_t Time, int Units, Duration DurationUnit);
+  
 
 } /* extern "C++"*/
 #endif /* __cplusplus*/
